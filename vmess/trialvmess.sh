058f77d4ddf419d5e5c4205a9fdbabffcff1c1be
v1nch3r <musarrum@gmail.com>
2023-12-26 18:15:41 +0700

    fix vmess doesn't work

--- vmess/trialvmess.sh
+++ vmess/trialvmess.sh
@@ -141,7 +141,7 @@ echo -e "id            : $uuid" | tee -a /user/log-vmess-$user.txt
 echo -e "AlterId       : 0" | tee -a /user/log-vmess-$user.txt
 echo -e "Security      : auto" | tee -a /user/log-vmess-$user.txt
 echo -e "Network       : Websocket" | tee -a /user/log-vmess-$user.txt
-echo -e "Path          : /(multipath) • ubah suka-suka" | tee -a /user/log-vmess-$user.txt
+echo -e "Path          : /vmess" | tee -a /user/log-vmess-$user.txt
 echo -e "ServiceName   : vmess-grpc" | tee -a /user/log-vmess-$user.txt
 echo -e "Alpn          : h2, http/1.1" | tee -a /user/log-vmess-$user.txt
 echo -e "————————————————————————————————————————————————————" | tee -a /user/log-vmess-$user.txt
